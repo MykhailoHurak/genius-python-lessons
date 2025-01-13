72ccfcec0c231c9bc56fcf1291442141616ad170
Mykhailo Hurak <mykhailohurak@gmail.com>
2025-01-13 19:36:18 +0200

    lesson 1-02

---
+++ lesson_1_02/lesson_1_02_01.py
@@ -0,0 +1,29 @@
+# Змінні
+
+Name = "Mike"
+name = 'Michael'
+_name = "Mykhailo"
+name_01 = 'Miguel'
+
+print(Name)
+print(name)
+print(_name)
+print(name_01)
+
+# Варіанти назв змінних under_score, camelCase
+
+# under_score
+user_email = "example1@gmail.com"
+print(user_email)
+
+# camelCase
+userEmail = "example2@gmail.com"
+print(userEmail)
+
+# Ім'я - це посилання в пам'яті і можна перевизначати значення змінної
+
+name = 'Mike'
+print(name)
+
+name = 'Michael'
+print(name)
---
+++ lesson_1_02/lesson_1_02_02.py
@@ -0,0 +1,110 @@
+# Оператори
+
+# Додавання
+num_1 = 100
+num_2 = 10
+result = num_1 + num_2
+print(result)
+
+# Віднімання
+num_1 = 100
+num_2 = 10
+result = num_1 - num_2
+print(result)
+
+# Множення
+num_1 = 100
+num_2 = 10
+result = num_1 * num_2
+print(result)
+
+# Ділення
+num_1 = 100
+num_2 = 10
+result = num_1 / num_2
+print(result)
+
+# Ділення звичайне та цілочисельне
+num_1 = 7
+num_2 = 2
+
+num_3 = num_1 / num_2
+num_4 = num_1 // num_2
+
+print(num_3)
+print(num_4)
+
+# Возведення числа у ступінь
+num_1 = 5
+num_2 = 2
+
+num_3 = num_1 ** num_2
+print(num_3)
+
+# Залишок від ділення
+num_1 = 7
+num_2 = 2
+
+num_3 = num_1 % num_2
+print(num_3)
+
+# Оператори порівняння == Дорівнює
+num_1 = 7
+num_2 = 2
+
+num_3 = num_1 == num_2
+print(num_3)
+
+# Оператори порівняння != Не дорівнює
+num_1 = 7
+num_2 = 2
+
+num_3 = num_1 != num_2
+print(num_3)
+
+# Оператори порівняння < Менше
+num_1 = 7
+num_2 = 2
+
+num_3 = num_1 < num_2
+print(num_3)
+
+# Оператори порівняння > Більше
+num_1 = 7
+num_2 = 2
+
+num_3 = num_1 > num_2
+print(num_3)
+
+# Оператор Логічний and
+num = 10
+name = "Mike"
+
+result = num > 5 and name == "Mike"
+print(result)
+
+# Оператор Логічний or
+num = 10
+name = "Mike"
+
+result = num < 5 or name == "Mike"
+print(result)
+
+# Оператор Логічний in, not in
+num = 10
+name = "Mike"
+message_1 = "Mike got some money"
+message_2 = "You won"
+
+print(name in message_1)
+print(name not in message_1)
+
+print(name in message_2)
+print(name not in message_2)
+
+# Комбінуємо
+name = "Mike"
+age = 33
+animal = "dog"
+
+print(name = "Mike" and age > 18 and animal != "cat")
