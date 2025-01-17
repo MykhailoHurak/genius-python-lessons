dbefc92da6191b1b28d35f7bd1b2335eec0d92fd
Mykhailo Hurak <mykhailohurak@gmail.com>
2025-01-18 00:13:32 +0200

    lesson_1_02

--- lesson_1_02/lesson_1_02_03.py
+++ lesson_1_02/lesson_1_02_03.py
@@ -1,8 +1,33 @@
-# Змінні
-# Ім'я - це посилання в пам'яті і можна перевизначати значення змінної
+# Типи данних
 
-name = 'Mike'
-print(name)
+num_1 = 5
+print(type(num_1))
 
-name = 'Michael'
-print(name)
+num_2 = 3.14
+print(type(num_2))
+
+string = "Real"
+print(type(string))
+
+example_check = True
+print(type(example_check))
+
+example_list = ["My", "name", "is", "Michael"]
+print(type(example_list))
+
+example_tuple = (1, 2, 3)
+print(type(example_tuple))
+
+example_dictionary = {"name": "Mykhailo", "age": 33}
+print(type(example_dictionary))
+
+example_set = {1, 2, 3, 4}
+print(type(example_set))
+
+print(None)
+
+class Person:
+    pass
+
+a = Person()
+print(type(a))
